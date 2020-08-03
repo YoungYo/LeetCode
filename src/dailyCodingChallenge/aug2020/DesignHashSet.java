6e8edbd0034cbd253ee72646fb617b2b6f60e928
wanghao20 <wanghao20@baijiahulian.com>
2020-08-03 13:37:26 +0800

    每日一题：Design HashSet

---
+++ src/dailyCodingChallenge/aug2020/DesignHashSet.java
@@ -0,0 +1,46 @@
+package dailyCodingChallenge.aug2020;
+
+public class DesignHashSet {
+    public static void main(String[] args) {
+        MyHashSet hashSet = new MyHashSet();
+        hashSet.add(1);
+        hashSet.add(2);
+        System.out.println(hashSet.contains(1));    // returns true
+        System.out.println(hashSet.contains(3));    // returns false (not found)
+        hashSet.add(2);
+        System.out.println(hashSet.contains(2));    // returns true
+        hashSet.remove(2);
+        System.out.println(hashSet.contains(2));    // returns false (already removed)
+    }
+}
+
+class MyHashSet {
+
+    private boolean[] arr;
+    /** Initialize your data structure here. */
+    public MyHashSet() {
+        this.arr = new boolean[1000001];
+    }
+
+    public void add(int key) {
+        arr[key] = true;
+    }
+
+    public void remove(int key) {
+        arr[key] = false;
+    }
+
+    /** Returns true if this set contains the specified element */
+    public boolean contains(int key) {
+        return arr[key];
+    }
+}
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * MyHashSet obj = new MyHashSet();
+ * obj.add(key);
+ * obj.remove(key);
+ * boolean param_3 = obj.contains(key);
+ */
+
