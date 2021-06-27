feca6ec6ba7f44a825cebf803cb103004cceaedd
wanghao20 <wanghao20@baijia.com>
2021-06-27 12:26:18 +0800

    Add the 198th question: House Robber

---
+++ src/leetcode/editor/en/HouseRobber.java
@@ -0,0 +1,61 @@
+//You are a professional robber planning to rob houses along a street. Each hous
+//e has a certain amount of money stashed, the only constraint stopping you from r
+//obbing each of them is that adjacent houses have security systems connected and 
+//it will automatically contact the police if two adjacent houses were broken into
+// on the same night. 
+//
+// Given an integer array nums representing the amount of money of each house, r
+//eturn the maximum amount of money you can rob tonight without alerting the polic
+//e. 
+//
+// 
+// Example 1: 
+//
+// 
+//Input: nums = [1,2,3,1]
+//Output: 4
+//Explanation: Rob house 1 (money = 1) and then rob house 3 (money = 3).
+//Total amount you can rob = 1 + 3 = 4.
+// 
+//
+// Example 2: 
+//
+// 
+//Input: nums = [2,7,9,3,1]
+//Output: 12
+//Explanation: Rob house 1 (money = 2), rob house 3 (money = 9) and rob house 5 
+//(money = 1).
+//Total amount you can rob = 2 + 9 + 1 = 12.
+// 
+//
+// 
+// Constraints: 
+//
+// 
+// 1 <= nums.length <= 100 
+// 0 <= nums[i] <= 400 
+// 
+// Related Topics Array Dynamic Programming 
+// 👍 7423 👎 200
+
+  
+package leetcode.editor.en;
+public class HouseRobber {
+    public static void main(String[] args) {
+         Solution solution = new HouseRobber().new Solution();
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public int rob(int[] nums) {
+            int len = nums.length;
+            int prePre = 0, pre = nums[0], result = nums[0];
+            for (int i = 1; i < len; i++) {
+                result = Math.max(pre, prePre + nums[i]);
+                prePre = pre;
+                pre = result;
+            }
+            return result;
+        }
+    }
+    //leetcode submit region end(Prohibit modification and deletion)
+}
