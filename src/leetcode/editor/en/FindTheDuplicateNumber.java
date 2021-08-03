6d4217a4f157f04ae1e893080950d4a2a9c6cae1
wanghao <wanghao19961026@163.com>
2021-08-03 23:21:07 +0800

    完成了题解：287. Find the Duplicate Number

--- src/leetcode/editor/en/FindTheDuplicateNumber.java
+++ src/leetcode/editor/en/FindTheDuplicateNumber.java
@@ -45,23 +45,41 @@
 package leetcode.editor.en;
 public class FindTheDuplicateNumber {
     public static void main(String[] args) {
-         Solution solution = new FindTheDuplicateNumber().new Solution();
+        Solution solution = new FindTheDuplicateNumber().new Solution();
+        int[][] inputs = {
+                {1,3,4,2,2},
+                {2,2,2,2,2},
+                {3,1,3,4,2},
+                {1,1},
+                {1,1,2}
+        };
+        for (int[] input: inputs) {
+            System.out.println(solution.findDuplicate(input));
+        }
     }
     //leetcode submit region begin(Prohibit modification and deletion)
-    // 根据异或的两个特点，任何两个相同的数异或的结果都为0，任何数与0异或都为这个数，
-    // 因此将所有的数依次异或得到的结果就是除了两个重复数的所有数的异或结果，假设为T。
-    // 而将1到n依次异或的结果为T^重复数。因此，重复数=T^T^重复数。
-    // 即:所有数异或的结果再异或1到n所有数异或的结果
     class Solution {
         public int findDuplicate(int[] nums) {
-            int t = 0;
-            for (int i : nums) {
-                t ^= i;
+            int p = nums[0], q = nums[nums[0]];
+            while (p != q) {
+                p = nums[p];
+                q = nums[nums[q]];
             }
-            for (int i = 0; i < nums.length; i++) {
-                t ^= i;
+            int cycleLength = 1;
+            p = nums[p];
+            while (p != q) {
+                p = nums[p];
+                cycleLength++;
             }
-            return t;
+            int start = nums[0], end = 0;
+            for (int i = 0; i < cycleLength; i++) {
+                end = nums[end];
+            }
+            while (nums[end] != start) {
+                start = nums[start];
+                end = nums[end];
+            }
+            return start;
         }
     }
     //leetcode submit region end(Prohibit modification and deletion)
